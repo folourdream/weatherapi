06deae87e6fbe2155195e1c366b5770e1c4684e5
scrumster <scrumster77@gmail.com>
2017-08-24 13:11:46 +1000

    refactored

--- apinodejs_weather_fp.js
+++ apinodejs_weather_fp.js
@@ -12,71 +12,60 @@ var options = {
 	method: 'GET'
 }
 
-var body = '';
-var weather ='';
-var fileContent = {};
 
 
-var readData = function(chunk){
-
-	body += chunk;
-	console.log(chunk);
-	console.log(body);
-};
-
-var iAmDone = function() {
-	var detail = JSON.parse(body);
-	console.log(detail.sys.country);
-	// var country =  detail.sys;
-	// console.log(country.sunrise);
-	console.log(fileContent);
-   	console.log(fileContent.main.temp_max);
-}
-
-var makeaCall = function(){
-	
-
-		var climate = JSON.parse();
-		var  currenttemp=  weather.main.temp_max;
-		console.log(currenttemp);
-
-	//var currenttemp = Json.parse(weather.main.temp_max;
-        //fs.writeFile('test.json',JSON.stringify(temp_max),'utf8');
-
-        console.log(currenttemp);
-        if(currenttemp < 10)
-
-        {
-          console.log("bloody cold day");
-        }
-        else
-        {
-          console.log("pleasant day");
-        }
-}
-
-var makeSalesDecision = function(){
-
-	fs.readData('./test.json',makeaCall());
-
-}
-
-var writeToFile = function(){
-
-	var weather = JSON.parse(body);
-   	fs.writeFile('test.json',weather,'utf8');
-   	fileContent = weather;
+http.request(options, function(res) {
+	var content = '';
+	res.on('data', function(chunk) {
+		content += chunk;
+	});
+	res.on('end', function() {
+		var obj = JSON.parse(content);
+		console.log(obj.main.temp_max);
+	});
+}).end();
 
 
-    console.log('test.json');
-}
 
-var handleResponse = function(res) {
 
-	res.on('data', readData);
-	res.on('data', writeToFile);
-	res.on('end', iAmDone);
 
-};
+// http.request(options, function(res) 
+// 				{
+// 					var body = '';
+// 					res.on('data', function(chunk)
+// 					{
+// 						//var body = '';
+// 						body += chunk;
+// 						//console.log(chunk);
+// 						console.log(body);
+// 					}
+// 				)
+			
+// 			// 		res.on('end', function()
+// 			// 	{
 
-http.request(options, handleResponse).end();
+// 			// 		var weather = JSON.parse(body);
+// 			// 	   	fs.writeFile('test.json',weather,'utf8');
+// 			// 	   	fileContent = weather;
+
+
+// 			// 	}
+// 			// )
+// 				res.on('end', function() 
+
+
+// 					{
+// 						var filecontent ={};
+// 						fileContent = JSON.parse(body);
+// 					console.log(fileContent.main.temp_max);
+// 					// var country =  detail.sys;
+// 					// console.log(country.sunrise);
+// 					console.log(fileContent);
+// 				   	console.log(fileContent.main.temp_max);
+// 					}
+// 				);
+			
+
+// 				}
+// 			).end();
+
